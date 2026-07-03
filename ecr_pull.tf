24662f947efaa68f1c30c3a6484ba2d2b787a26b
rspoovin91 <60533291+rspoovin91@users.noreply.github.com>
2026-07-03 08:41:49 +0100

    Create ecr_pull.tf

---
+++ ecr_pull.tf
@@ -0,0 +1,41 @@
+resource "aws_ecr_pull_through_cache_rule" "ecr_public" {
+  ecr_repository_prefix = "ecr-public"
+  upstream_registry_url = "public.ecr.aws"
+}
+
+resource "aws_iam_role_policy" "ecr_pull_through" {
+  name = "ecr-pull-through-cache"
+  role = aws_iam_role.eks_node.name # your existing node role
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "PullThroughCache"
+        Effect = "Allow"
+        Action = [
+          "ecr:CreateRepository",
+          "ecr:BatchImportUpstreamImage"
+        ]
+        Resource = "arn:aws:ecr:${var.region}:${data.aws_caller_identity.current.account_id}:repository/ecr-public/*"
+      }
+    ]
+  })
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = aws_eks_cluster.this.name
+  addon_name   = "vpc-cni"
+  addon_version = "v1.18.1-eksbuild.1"
+
+  configuration_values = jsonencode({
+    init = {
+      image = {
+        repository = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com/ecr-public/eks-distro/kubernetes/pause"
+      }
+    }
+    image = {
+      repository = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com/ecr-public/amazon-vpc-cni"
+    }
+  })
+}
